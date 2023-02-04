d602cec38d818c746836866f69e1ebf302c29a86
Skyron92 <courtier.leo@gmail.com>
2023-02-04 17:55:56 +0100

    Menu WIP

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,9 +1,21 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Toggle windowed;
+
+    private void Start() {
+        windowed = GetComponent<Toggle>();
+    }
+
+    private void Update() {
+        if (windowed.isOn) Screen.fullScreenMode = FullScreenMode.Windowed;
+    }
+    
+
     public void Retry() {
         SceneManager.LoadScene("SampleScene");
     }
@@ -13,4 +25,5 @@ public class Menu : MonoBehaviour
         EditorApplication.isPlaying = false;
         Application.Quit();
     }
+    
 }
