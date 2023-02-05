cfa84d9dc0ba80ee4069aea35b76b6624eae9523
Skyron92 <courtier.leo@gmail.com>
2023-02-05 18:15:32 +0100

    Petite Modif

---
+++ Assets/Scripts/Player/WaterDrop.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using UnityEngine;
+
+public class WaterDrop : MonoBehaviour
+{
+    public ParticleSystem drop;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")) drop.Play();
+    }
+}
