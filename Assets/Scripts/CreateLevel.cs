a316463656c5114e077472e57ed643f631ed7903
allan <allan@nineteen-squared.com>
2023-02-05 13:10:54 +0100

    Update lvl generation

--- Assets/Scripts/CreateLevel.cs
+++ Assets/Scripts/CreateLevel.cs
@@ -25,27 +25,33 @@ public class CreateLevel : MonoBehaviour
         List<GameObject> isDropletOnBlock = level;
 
         if(mapSize > 0){
-
+        //Je veux creer une terrain avec un size
         for(int i = 0; i < mapSize; i++){
-
+            //Je prendre un element au pif dans ma liste
             int rand = Random.Range(0, level.Count);
+            //Je jette un "dé" pour savoir si le terrain aura une goutte ou non
             int isDroplet = Random.Range(1,100);
-
+            //Si le terrain à une goutte, je change de liste, et d'element prit au pif.
             if(isDroplet < 20){
                isDropletOnBlock = levelWithDroplet;
                rand = Random.Range(0,isDropletOnBlock.Count);
             }
-            
+            //Si l'élement prit au pif est valide
             if(IsLevelValid(isDropletOnBlock[rand], displayedLevel)){
-                
+                //J'instancie le block
                 Instantiate(isDropletOnBlock[rand], new Vector3((i+1)*blockWidth, 0, 0), Quaternion.Euler(-90f, 0f, 0f));
+                //Si le block instancier est une plateform, je lui ajoute le sol
                 if(isDropletOnBlock[rand].name.Contains("Platform")){ 
                     Instantiate(ground, new Vector3((i+1)*blockWidth, 0, 0), Quaternion.Euler(-90f, 0f, 0f));
                 }
-            }else{
+            }
+            //Si il n'est pas valide, je refais un tour
+            else{
                     i--;
                 }
-
+            //Je garde en mémoire le dernier block placé pour verifier que le prochain à placer est valide. 
+            Debug.Log(isDropletOnBlock);
+            Debug.Log(rand);
             displayedLevel = isDropletOnBlock[rand];
         }
         }
