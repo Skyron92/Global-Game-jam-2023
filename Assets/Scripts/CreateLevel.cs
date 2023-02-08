93bca906dc2b91c81cd87c462eb9359e2c44a211
allan <allan@nineteen-squared.com>
2023-02-08 01:47:57 +0100

    Refactor mapcreation to avoid Double ronce
    Resize Ronce

--- Assets/Scripts/CreateLevel.cs
+++ Assets/Scripts/CreateLevel.cs
@@ -7,16 +7,22 @@ public class CreateLevel : MonoBehaviour
     public List<GameObject> level;
     public List<GameObject> levelWithDroplet;
     public List<GameObject> levelWithRonce;
+
     public GameObject ground;
+
     public int mapSize;
+    [Range(0,50)][SerializeField] private int difficulty;
     private int blockWidth = 5;
     private MeshRenderer levelSize;
+
     public GameObject start;
     public GameObject finish;
     public GameObject droplet;
     public GameObject playerGO;
 
     private Vector3 initialPosition = new Vector3(0, 1.5f, 0);
+    private GameObject displayedLevel;
+    private List<GameObject> currentList;
 
     
     public static float remainingTime = 15;
@@ -25,8 +31,9 @@ public class CreateLevel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        generateLevel();
-        startGame();
+        displayedLevel = start;
+        GenerateLevel();
+        StartGame();
      
     }     
 
@@ -35,40 +42,44 @@ public class CreateLevel : MonoBehaviour
         return (toDisplay.name[0] == lastDisplayed.name[1]);
     }
 
-    private void generateLevel(){
-        GameObject displayedLevel = start;
-        List<GameObject> isDropletOnBlock = level;
-
+    private void GenerateLevel(){
         if(mapSize > 0){
-            
-        for(int i = 0; i < mapSize; i++){
-            int rand = Random.Range(0, level.Count);
-            int isDroplet = Random.Range(1,100);
-            if(isDroplet < 20){
-               isDropletOnBlock = levelWithDroplet;
-               rand = Random.Range(0,isDropletOnBlock.Count);
-            }
-            if(isDroplet <= 30 && isDroplet >= 20){
-                isDropletOnBlock = levelWithRonce;
-                rand = Random.Range(0,isDropletOnBlock.Count);
-            }
-            if(IsLevelValid(isDropletOnBlock[rand], displayedLevel)){
-                Instantiate(isDropletOnBlock[rand], new Vector3((i+1)*blockWidth, 0, 0), Quaternion.Euler(-90f, 0f, 0f));
-                if(isDropletOnBlock[rand].name.Contains("Platform")){ 
-                    Instantiate(ground, new Vector3((i+1)*blockWidth, 0, 0), Quaternion.Euler(-90f, 0f, 0f));
-                }
-            }
-            else{
+             for(int i = 0; i < mapSize; i++){
+                GameObject blockToDisplay = SelectBlock();
+               if(IsLevelValid(blockToDisplay, displayedLevel)){
+                    Instantiate(blockToDisplay, new Vector3((i+1)*blockWidth, 0, 0), Quaternion.Euler(-90f, 0f, 0f));
+                    if(blockToDisplay.name.Contains("Platform")){ 
+                        Instantiate(ground, new Vector3((i+1)*blockWidth, 0, 0), Quaternion.Euler(-90f, 0f, 0f));
+                    }
+                    displayedLevel = blockToDisplay;
+                }else{
                     i--;
                 }
-            displayedLevel = isDropletOnBlock[rand];
-        }
+            }
         }
         Instantiate(finish, new Vector3((mapSize)*blockWidth+blockWidth, 0, 0), Quaternion.Euler(-90f, 0f, 0f));
     }
 
-    public void startGame(){
+    public void StartGame(){
         playerGO.transform.position = initialPosition;
         HydratationManager.currentValue = 100f;
     }
+
+    public GameObject SelectBlock(){
+        //lancer un dé
+        int randNumber = Random.Range(0,100);
+        if(randNumber <= difficulty && !displayedLevel.name.Contains("Ronce")){
+            currentList = levelWithRonce;
+        }
+        if(randNumber > difficulty && randNumber <= 50 && !displayedLevel.name.Contains("Droplet")){
+            currentList = levelWithDroplet;
+        }
+        else{
+            currentList = level;
+    }
+
+    int randBlockFromList = Random.Range(0, currentList.Count);
+    return currentList[randBlockFromList];
+    }
+    
 }
